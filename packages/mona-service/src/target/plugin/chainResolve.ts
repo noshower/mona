2239c9183c07b31e9265e6d061b3aab65033e60a
吴震宇 <wu.zhenyu@darcytech.com>
2022-09-03 14:42:58 +0800

    feat: 支持typescript paths 自动生成别名

--- packages/mona-service/src/target/plugin/chainResolve.ts
+++ packages/mona-service/src/target/plugin/chainResolve.ts
@@ -3,14 +3,19 @@ import { merge } from 'lodash';
 
 import Config from 'webpack-chain';
 import { TARGET } from './constants';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import { appSrcPath, appTsConfigPath } from './utils';
 
 const extensions = ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.json'];
 
 export function chainResolve(webpackConfig: Config, configHelper: ConfigHelper) {
   const resolve = webpackConfig.resolve;
   resolve.extensions.merge(extensions);
-  // resolve.modules.merge(['node_modules']);
+  // 解决 @import "~styles" 找不到模块问题
+  resolve.modules.merge([appSrcPath, 'node_modules']);
   resolve.alias.merge(merge(genAlias(), configHelper.projectConfig.abilities?.alias));
+
+  resolve.plugin('tsconfigPathsPlugin').use(TsconfigPathsPlugin, [{ configFile: appTsConfigPath }]);
 }
 
 export function genAlias() {
--- packages/mona-service/src/target/plugin/utils.ts
+++ packages/mona-service/src/target/plugin/utils.ts
@@ -2,6 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 const appDirectory = fs.realpathSync(process.cwd());
+
 const resolveApp = (relativePath: string) => path.resolve(appDirectory, relativePath);
 
 export const appSrcPath = resolveApp('src');
+
+export const appTsConfigPath = resolveApp('tsconfig.json');
